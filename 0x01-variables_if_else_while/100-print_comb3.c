5c6aeaddd7b927631273dcf365e693fbdf6d5d2f
SnymanD <dhlaminisnyman@gmail.com>
2023-03-17 23:55:05 +0000

    Uhm the Advanced task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * Description: main - Print different combinations.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d, r;
+
+	for (d = '0'; d < '9'; d++)
+	{
+	for (r = d + 1; r <= '9'; r++)
+	{
+		if (r != d)
+	{
+		putchar(d);
+		putchar(r);
+
+		if (d == 'a' && r == '9')
+		continue;
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
