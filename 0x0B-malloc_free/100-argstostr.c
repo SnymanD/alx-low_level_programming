4a8db5656392f0bc82f547b9aaca4218897ac483
SnymanD <dhlaminisnyman@gmail.com>
2023-04-11 19:38:37 +0000

    Fixed errors

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,41 +11,41 @@
 
 char *argstostr(int ac, char **av)
 {
-        int ch = 0;
-        int x = 0, y = 0, z = 0;
-        char *s;
+	int ch = 0;
+	int x = 0, y = 0, z = 0;
+	char *s;
 
-        if (ac == 0 || av == NULL)
-                return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-        while (x < ac)
-        {
-                while (av[x][y])
-                {
-                        ch++;
-                        y++;
-                }
-                        y = 0;
-                        x++;
-        }
-                s = malloc((sizeof(char) * ch) + ac + 1);
+	while (x < ac)
+	{
+		while (av[x][y])
+		{
+			ch++;
+			y++;
+		}
+			y = 0;
+			x++;
+	}
+		s = malloc((sizeof(char) * ch) + ac + 1);
 
-                x = 0;
+		x = 0;
 		while (av[x])
-                {
-                        while (av[x][y])
-                        {
-                                s[z] = av[x][y];
-                                z++;
-                                y++;
-                        }
-                        s[z] = '\n';
+		{
+			while (av[x][y])
+			{
+				s[z] = av[x][y];
+				z++;
+				y++;
+			}
+			s[z] = '\n';
 
-                        y = 0;
-                        z++;
-                        x++;
-                }
-                        z++;
-                        s[z] = '\0';
-                        return (s);
+			y = 0;
+			z++;
+			x++;
+		}
+			z++;
+			s[z] = '\0';
+			return (s);
 }
