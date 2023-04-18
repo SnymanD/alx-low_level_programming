3202b78cd751e9e18f7dd224d00376d308210a78
SnymanD <dhlaminisnyman@gmail.com>
2023-04-18 16:14:06 +0000

    All the tasks solved

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: string to evaluate
+ * Return: String length
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+
+	return (x);
+}
+
+/**
+ * *_strcpy - Copies the string pointed
+ * @dest: Destination
+ * @src: string to be copied
+ * Return: Pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0, x;
+
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (x = 0; x < len; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[x] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - Creates a new dog
+ * @name: Name of the new dog
+ * @age: Age of the new dog
+ * @owner: Owner of the new dog
+ * Return: Pointer to the new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int len1, len2;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A type that describes a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+
+struct dog
+{
+	char *name, *owner;
+	float age;
+};
+
+/**
+ * dog_t - typedef of struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
