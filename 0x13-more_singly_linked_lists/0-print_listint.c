9fdbeddd3d432b70e94de351f938e2a5f041f0ad
SnymanD <dhlaminisnyman@gmail.com>
2023-05-03 18:09:50 +0000

    Fixed the first error

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,5 +11,5 @@ size_t print_listint(const listint_t *h)
 	if (h == NULL)
 		return (0);
 	printf("%d\n", h->n);
-	return (1 + print_listint(h->adjacent));
+	return (1 + print_listint(h->next));
 }
